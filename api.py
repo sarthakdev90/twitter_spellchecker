741d12835c27b68168182f38d89f32c83f081dca
sarthakdev90 <sarthakdev90@gmail.com>
2014-09-19 17:41:04 +0530

    Added class methods for processing a tweet

--- api.py
+++ api.py
@@ -13,33 +13,58 @@ class TwitterStreamListener(tweepy.StreamListener):
 	'''
 		Listener class which listens to the Streaming API
 	'''
-
-	def on_data(self, tweet):
+	
+	def process(self, tweet):
+		'''
+			Function to process a tweet.
+		'''
 		try:
 		    t = json.loads(tweet).get('text').encode('utf-8')
 	            print "Tweet: ", t
 		    numbers_db.incr("tweets")
 		    if t:
 		        sent = TextBlob(t)
-		        words = sent.words
+		        words = [i for i in sent.words if i != 'RT' and not i.startswith('@') and not self.is_link(i)]
+		        print "Words: ", words
 			for w in words:
 			    numbers_db.incr("words")
 			    wor = Word(w)
 			    if len(wor.spellcheck()) != 1:
 			        incorrect_words_db.rpush("words", w)
-			        numbers_db.incr("mistakes")
-			#Do something with the on_data
+			return True
+			
+		except Exception as e:
+			print traceback.format_exc(e)
+			return False
+			
+	def is_link(self, token):
+		'''
+			Function to check whether the given token is a link or not
+		'''
+		return re.match('http(s){0,1}://.+\..+', token, re.I) is not None
+			
+			
+	def on_data(self, tweet):
+		'''
+			This function gets called whenever the Streaming API finds data
+		'''
+		try:
+		    self.process(tweet)
 		    mistakes = incorrect_words_db.lrange("words", 0, -1)
 		    counter = Counter(mistakes)
 		    
 		    print "Crunched Tweets: ", numbers_db.get("tweets")
 		    print "Crunched Words: ", numbers_db.get("words") 
-		    print "Corrected: ", numbers_db.get("mistakes")
+		    print "Corrected: ", incorrect_words_db.llen("mistakes")
 		    print "Most common: ", counter.most_common()[0][0]
+		    
 		except Exception as e:
 			print traceback.format_exc()
 
 	def on_error(self, status):
+		'''
+			This function gets called whenever the Streaming API encounters an error
+		'''
 		print "Error: ", status
 
 
