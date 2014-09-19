f52fc292bf13231d02b83cec881d469200247d13
sarthak <sarthak@devopstest5.devopstest5.i5.internal.cloudapp.net>
2014-09-19 17:29:36 +0530

    WIP: Processing tweets

--- api.py
+++ api.py
@@ -2,11 +2,12 @@ from textblob import TextBlob, Word
 from twitter_config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_KEY
 from tweepy import Stream, OAuthHandler
 from tweepy.streaming import StreamListener
-import tweepy
-import json
+import tweepy, redis
+import json, traceback
+from collections import Counter
 
-crunched_words = 0
-corrected = 0
+numbers_db = redis.Redis(db = 10)
+incorrect_words_db = redis.Redis(db = 11)
 
 class TwitterStreamListener(tweepy.StreamListener):
 	'''
@@ -14,30 +15,43 @@ class TwitterStreamListener(tweepy.StreamListener):
 	'''
 
 	def on_data(self, tweet):
-	    t = json.loads(tweet).get('text').encode('utf-8')
-            print "Tweet: ", t
-	    if t:
-	        sent = TextBlob(t)
-	        words = sent.words
-		for w in words:
-		    global crunched_words, corrected
-		    crunched_words += 1
-		    wor = Word(w)
-		    if len(wor.spellcheck()) != 1:
-		        corrected += 1
-		#Do something with the on_data
-	    print "Crunched Words: ", crunched_words
-	    print "Corrected: ", corrected
+		try:
+		    t = json.loads(tweet).get('text').encode('utf-8')
+	            print "Tweet: ", t
+		    numbers_db.incr("tweets")
+		    if t:
+		        sent = TextBlob(t)
+		        words = sent.words
+			for w in words:
+			    numbers_db.incr("words")
+			    wor = Word(w)
+			    if len(wor.spellcheck()) != 1:
+			        incorrect_words_db.rpush("words", w)
+			        numbers_db.incr("mistakes")
+			#Do something with the on_data
+		    mistakes = incorrect_words_db.lrange("words", 0, -1)
+		    counter = Counter(mistakes)
+		    
+		    print "Crunched Tweets: ", numbers_db.get("tweets")
+		    print "Crunched Words: ", numbers_db.get("words") 
+		    print "Corrected: ", numbers_db.get("mistakes")
+		    print "Most common: ", counter.most_common()[0][0]
+		except Exception as e:
+			print traceback.format_exc()
 
 	def on_error(self, status):
 		print "Error: ", status
 
 
 if __name__ == '__main__':
-	auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-	auth.set_access_token(ACCESS_KEY, ACCESS_TOKEN)
-	
-	l = TwitterStreamListener()
-	streamer = Stream(auth, listener = l)
-	streamer.filter(track = ['#mufc'])
+	try:
+		auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+		auth.set_access_token(ACCESS_KEY, ACCESS_TOKEN)
+		
+		l = TwitterStreamListener()
+		streamer = Stream(auth, listener = l)
+		streamer.filter(track = ['#mufc'])
+
+	except Exception as e:
+		print traceback.format_exc(e)
 
